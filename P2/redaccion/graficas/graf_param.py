5dd097dda4c437ea57e111fee5638290bf6a956b
Mapachana <anabuenrua99@gmail.com>
2021-12-11 01:07:50 +0100

    Añadidas graficas parametros

---
+++ P2/redaccion/graficas/graf_param.py
@@ -0,0 +1,41 @@
+from matplotlib import pyplot as plt
+
+
+
+fichero_leer = "3_meanshift.csv"
+
+silhouette = []
+ch = []
+param = []
+lineas = []
+with open(fichero_leer, "r") as fichero:
+    lineas = fichero.readlines()
+
+aux = lineas[0].split(",")
+parametro = aux[1]
+lineas.pop(0)
+for linea in lineas:
+    aux = linea.split(",")
+
+    if aux[-2] != "" and aux[-1] != "":
+        silhouette.append(float(aux[-2]))
+        ch.append(float(aux[-3]))
+        param.append(float(aux[1]))
+        
+print(param)
+print(silhouette)
+print(ch)
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.plot(param,ch, marker='x')
+ax1.set_title('Calinski-Harabasz')
+ax1.set_xlabel(parametro)
+ax1.set_ylabel('Calinski-Harabasz')
+ax2.plot(param,silhouette, marker='x')
+ax2.set_title('Silhouette')
+ax2.set_xlabel(parametro)
+ax2.set_ylabel('Silhouette')
+
+fig.legend()
+fig.tight_layout(pad=3.0)
+fig.savefig('3_parametros_meanshift.eps')
---
+++ P2/redaccion/graficas/graf_param_birch.py
@@ -0,0 +1,94 @@
+from matplotlib import pyplot as plt
+
+
+
+fichero_leer = "1_param_birch.csv"
+
+silhouette1 = []
+ch1 = []
+silhouette2 = []
+ch2 = []
+silhouette3 = []
+ch3 = []
+silhouette4 = []
+ch4 = []
+silhouette5 = []
+ch5 = []
+param1 = []
+param2 = []
+param3 = []
+param4 = []
+param5 = []
+lineas = []
+with open(fichero_leer, "r") as fichero:
+    lineas = fichero.readlines()
+
+aux = lineas[0].split(",")
+parametro = aux[2]
+fijo = aux[1]
+parametro1 = 15
+parametro2 = 20
+parametro3 = 25
+parametro4 = 30
+parametro5 = 35
+lineas.pop(0)
+
+for linea in lineas:
+    aux = linea.split(",")
+
+    if aux[-1] != "" and aux[-2] != "":
+        if float(aux[1]) == parametro1:
+            silhouette1.append(float(aux[-2]))
+            ch1.append(float(aux[-3]))
+            param1.append(float(aux[2]))
+        elif float(aux[1]) == parametro2:
+            silhouette2.append(float(aux[-2]))
+            ch2.append(float(aux[-3]))
+            param2.append(float(aux[2]))
+        elif float(aux[1]) == parametro3:
+            silhouette3.append(float(aux[-2]))
+            ch3.append(float(aux[-3]))
+            param3.append(float(aux[2]))
+        elif float(aux[1]) == parametro4:
+            silhouette4.append(float(aux[-2]))
+            ch4.append(float(aux[-3]))
+            param4.append(float(aux[2]))
+        elif float(aux[1]) == parametro5:
+            silhouette5.append(float(aux[-2]))
+            ch5.append(float(aux[-3]))
+            param5.append(float(aux[2]))
+
+
+
+parametro1 = str(parametro1)
+parametro2 = str(parametro2)
+parametro3 = str(parametro3)
+parametro4 = str(parametro4)
+parametro5 = str(parametro5)
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.plot(param1,ch1, marker='x', label=fijo+"= "+parametro1)
+ax1.plot(param2,ch2, marker='x', label=fijo+"= "+parametro2)
+ax1.plot(param3,ch3, marker='x', label=fijo+"= "+parametro3)
+ax1.plot(param4,ch4, marker='x', label=fijo+"= "+parametro4)
+ax1.plot(param5,ch5, marker='x', label=fijo+"= "+parametro5)
+
+ax1.set_title('Calinski-Harabasz')
+ax1.set_xlabel(parametro)
+ax1.set_ylabel('Calinski-Harabasz')
+#ax1.legend(bbox_to_anchor=(0.5, -0.1))
+
+ax2.plot(param1,silhouette1, marker='x', label=fijo+"= "+parametro1)
+ax2.plot(param2,silhouette2, marker='x', label=fijo+"= "+parametro2)
+ax2.plot(param3,silhouette3, marker='x', label=fijo+"= "+parametro3)
+ax2.plot(param4,silhouette4, marker='x', label=fijo+"= "+parametro4)
+ax2.plot(param5,silhouette5, marker='x', label=fijo+"= "+parametro5)
+
+ax2.set_title('Silhouette')
+ax2.set_xlabel(parametro)
+ax2.set_ylabel('Silhouette')
+#ax2.legend(bbox_to_anchor=(0.5, -0.1))
+
+fig.legend(loc='lower left', bbox_to_anchor= (0.0, -0.2), ncol=2, borderaxespad=0, frameon=False)
+fig.tight_layout(pad=3.0)
+fig.savefig('parametros_birch.eps', bbox_inches='tight')
